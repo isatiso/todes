d1db9eaaf54389137ddefb062fd1078d1f33108b
曹嘉航 <caojiahang@formovie.cn>
2021-01-18 20:00:12 +0800

    更新文档。

--- src/redis-client.ts
+++ src/redis-client.ts
@@ -1167,6 +1167,8 @@ export class RedisClient extends BaseClient {
      *
      * 原子性的设置 key，并返回原值。如果 key 已经存在但值不是 string 类型，抛出异常。
      *
+     * **注意：**在 6.2 版本，此方法会被废弃。请使用 {@link RedisClient.set | SET} 命令以及 GET 选项。
+     *
      * @category String
      * @param key
      * @param value
@@ -1180,6 +1182,8 @@ export class RedisClient extends BaseClient {
      * > - **时间复杂度：**O(1)
      *
      * 原子性的设置 key，并返回原值。如果 key 已经存在但值不是 string 类型，抛出异常。
+     *
+     * **注意：**在 6.2 版本，此方法会被废弃。请使用 {@link RedisClient.set | SET} 命令以及 GET 选项。
      *
      * @param key
      * @param value
